25c4d2eb96a3e7095d2498a87aa699e2b526adb3
Lodewijk Nauta <lodewijk.nauta@surf.nl>
2023-08-22 11:23:33 +0200

    Fix slurm ex

--- examples/slurm-example.sh
+++ examples/slurm-example.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #SBATCH --array=0-5
 
-'''
-@helpdesk: SURF helpdesk <helpdesk@surf.nl>
 
-usage: sbatch slurm-example.sh
-description:
-    Connect to PiCaS server
-    Get the next token in todo View
-    Fetch the token parameters, e.g. input value
-    Run main job (process_task.sh) with the input argument
-    When done, return the exit code to the token
-    Attach the logs to the token
+#@helpdesk: SURF helpdesk <helpdesk@surf.nl>
+#
+#usage: sbatch slurm-example.sh
+#description:
+#    Connect to PiCaS server
+#    Get the next token in todo View
+#    Fetch the token parameters, e.g. input value
+#    Run main job (process_task.sh) with the input argument
+#    When done, return the exit code to the token
+#    Attach the logs to the token
 
-
-'''
 
 cd $PWD
 # You need to load your environment here
 # mamba activate MAMBA-ENV
+mamba activate snakemake-picas
 # source /PATH/TO/VENV/bin/activate
-./local-example.py
+python local-example.py
