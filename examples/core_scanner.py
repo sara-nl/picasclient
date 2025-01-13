70c8abcce6e69ff496653d1b62f0d0d7c5aac6b0
Lodewijk Nauta <lodewijk.nauta@surf.nl>
2025-01-13 10:26:42 +0100

    Fix fstring in core scanner[C

--- examples/core_scanner.py
+++ examples/core_scanner.py
@@ -22,7 +22,7 @@ client = CouchDB(url=picasconfig.PICAS_HOST_URL, db=picasconfig.PICAS_DATABASE, 
 work_avail = client.is_view_nonempty(args.view, design_doc=args.design_doc)
 if work_avail:
     picaslogger.info(f"Starting a picas clients checking view {args.view} in design document {args.design_doc}")
-    command = ["sbatch", "--cores {args.cores}", f"--export=VIEW={args.view},DOC={args.design_doc}", "N-core-job.sh"]
+    command = ["sbatch", f"--cores {args.cores}", f"--export=VIEW={args.view},DOC={args.design_doc}", "N-core-job.sh"]
     execute(command)
 else:
     picaslogger.info(f"Not starting a picas client, there is nothing to do in view {args.view} and for the designdocs {args.design_doc}.")
