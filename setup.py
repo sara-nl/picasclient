55e9dadfce3267e10fd88056f081bbd7d500da15
Mher Kazandjian <mherkazandjian@gmail.com>
2025-08-04 12:16:38 +0200

    add the installer setup.py script

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import pathlib
+import shutil
+from setuptools import setup
+from setuptools.command.install import install
+
+setup_cwd = pathlib.Path(__file__).parent.resolve()
+sys.path.insert(0, os.path.join(setup_cwd, '.'))
+from picas import metadata
+import picas
+
+setup(
+    name=metadata.package,
+    version=metadata.version,
+    description=metadata.description,
+    author=metadata.authors,
+    author_email=metadata.emails,
+    url=metadata.url,
+    packages=[
+        'picas',
+        'picas.apps',
+    ],
+    package_dir={
+        'picas': 'picas'
+    },
+    entry_points={
+        'console_scripts': [
+            'picas=picas.apps.picas:main',
+        ],
+    },
+#    cmdclass={
+#        'install': InstallCommand,
+#    }
+)
