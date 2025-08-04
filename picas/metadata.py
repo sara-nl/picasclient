0d0e748fa32399d3345cfcafcf31f100b0cc5d8b
Mher Kazandjian <mherkazandjian@gmail.com>
2025-08-04 12:15:31 +0200

    add missing fields to the metadata

--- picas/metadata.py
+++ picas/metadata.py
@@ -1,6 +1,7 @@
 # .. todo:: import some of the content from the pyproject.toml file
 project = 'picasclient'
-
+package = 'picasclient'
+description = "Python client using CouchDB as a token pool server."  # import from the toml file
 project_no_spaces = project.replace(' ', '')
 version = '1.1.0.dev0'  # import from the toml file
 authors = ["Jan Bot, Joris Borgdorff, Lodewijk Nauta, Haili Hu"]       # import from the toml fil
