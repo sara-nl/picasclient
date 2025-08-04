6ba2d067ad40be1d07394148270bdde57ebaedcd
Mher Kazandjian <mherkazandjian@gmail.com>
2025-08-04 11:54:24 +0200

    add the metadata file with the basic info

---
+++ metadata.py
@@ -0,0 +1,12 @@
+# .. todo:: import some of the content from the pyproject.toml file
+project = 'picasclient
+
+project_no_spaces = project.replace(' ', '')
+version = '1.1.0.dev0'  # import from the toml file
+authors = ["Jan Bot, Joris Borgdorff, Lodewijk Nauta, Haili Hu"]       # import from the toml fil
+
+authors_string = ', '.join(authors)
+emails = ['servicedesk.surf.nl']
+license = 'MIT'  # import from the toml file
+copyright = '2025 ' + authors_string
+url = 'https://github.com/sara-nl/picasclient'
